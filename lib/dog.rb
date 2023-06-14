d761a70c04057e576207501f15d67c186972da08
Antonio <antonio18casta@aol.com>
2023-06-14 16:11:08 -0400

    commit tree 9ddc226e0353037de3cc6561e7c36d67b41f1590

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
 
+    def name=(dog_name)
+        # We define an instance variable by prefacing the variable name with an @ symbol.
+        @this_dogs_name = dog_name
+    end
+
+    def name
+        @this_dogs_name
+    end
 end
+
+# class Dog
+#     # setter method
+#     def name=(value)
+#         # assign the @name instance variable a value of whatever is passed as an argument
+#         @name = value
+#     end
+
+#     # getter method
+#     def name
+#         # acces the data from the @name instance variable and return it
+#         @name
+#     end
+# end
